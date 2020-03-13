7cd7479cf70eecd7899042fc736f656416e482dd
Tyler Barabas <tylerbarabas@gmail.com>
2020-03-13 02:50:07 -0500

    export everything

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import Mixer from './src/mixer'
 import AudioPlayer from './src/audio-player'
 import DomElement from './src/dom-element'
 import GlobalStyle from './src/global-style'
+import Inspector from './src/inspector'
+import MasterController from './src/master-controller'
+import Mixer from './src/mixer'
+import Sequence from './src/sequence'
+import SpriteSheet from './src/sprite-sheet'
 import Stage from './src/stage'
 
-export { Mixer, DomElement, AudioPlayer, GlobalStyle, Stage }
+export {
+  AudioPlayer,
+  DomElement,
+  GlobalStyle,
+  Inspector,
+  MasterController,
+  Mixer,
+  Sequence,
+  SpriteSheet,
+  Stage,
+}
