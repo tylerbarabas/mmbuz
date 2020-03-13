7acdd95d045ff89993a7451a87795a2d3e1aa44a
Tyler Barabas <tylerbarabas@gmail.com>
2020-03-13 02:44:57 -0500

    shift things around...

---
+++ index.js
@@ -0,0 +1,7 @@
+import Mixer from './src/mixer'
+import AudioPlayer from './src/audio-player'
+import DomElement from './src/dom-element'
+import GlobalStyle from './src/global-style'
+import Stage from './src/stage'
+
+export { Mixer, DomElement, AudioPlayer, GlobalStyle, Stage }
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Mixer from './mixer'
-import AudioPlayer from './audio-player'
-import DomElement from './dom-element'
-import GlobalStyle from './global-style'
-
-export { Mixer, DomElement, AudioPlayer, GlobalStyle }
